6f2dd69b8348a86f86626aa824cd39249473083b
Alexander McDowell <lextmcdowell@gmail.com>
2024-07-01 21:33:23 -0700

    Initial commit

---
+++ model.py
@@ -0,0 +1,12 @@
+import time
+import sys
+import math
+
+import numpy as np
+
+import keras
+from keras import layers
+from keras import ops
+
+start_time = time.time()
+print(time.time() - start_time)
