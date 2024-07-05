3ede8151a73af961d9cf01951116b50878292b1e
Alexander McDowell <lextmcdowell@gmail.com>
2024-07-05 08:10:41 -0700

    Implement DBSCAN and homography attempt

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from os import listdir
-from os.path import isfile, join
+import json
 
 import math
 import numpy as np
@@ -21,6 +21,12 @@ import cv2 as cv
 # - CNN (100, 3, 3, 3) or ResNet
 # Piece Recognition:
 # - CNN (100, 3, 3, 3) or InceptionV3
+
+# Given the bottom-right and top-left points of a rectangle (p1 and p2 respectively), compute the matrix
+# mapping p1 to [s_x, 0] and p2 to [0, s_y]
+def compute_homography(p1, p2, s_x, s_y):
+    H_inv = np.asarray([[p1[0]/s_x, p2[0]/s_y], [p1[1]/s_x, p2[1]/s_y]])
+    return np.linalg.inv(H_inv)
 
 # Returns the slope-intercept components for a given line
 def slope_intercept(l):
@@ -58,27 +64,26 @@ def intersection_point(l1, l2):
 # labeled by the pieces (or lack thereof) which occupy that tile.
 # Returns a 64 element array of tuples corresponding to tiles to be passed into the piece classifier and the
 # corresponding label for that tile.
-def board_localization(image, fen):
+def board_localization(image, piece_data):
     height, width, _ = image.shape
     # Canny edge detector followed by a Hough Transformation to roughly find all of the lines in the image.
-    edges = cv.Canny(image, 200, 400)
+    edges = cv.Canny(image, 250, 400, apertureSize = 3)
     # Probabilistic Hough Transform
-    hough_lines = cv.HoughLinesP(edges, 1, np.pi / 180, 50, None, 50, 10)
-    
+    hough_lines = cv.HoughLinesP(edges, 1, np.pi / 180, 50, 80, 30, 10)
+
     # Plot the lines on the original image
-    scale_factor = 20
-    lines, angles = [], []
+    scale_factor = 25
+    lines = []
     for i in range(len(hough_lines)):
         l = hough_lines[i][0]
         # Line is given by (1 - t) * (l[0], l[1]) + t * (l[2], l[3]) for t = [-99, 99]
         # So extend line by doing t in [-scale_factor + 1, scale_factor]
         p1 = (scale_factor * l[0] - (scale_factor-1) * l[2], scale_factor * l[1] - (scale_factor-1) * l[3])
         p2 = (-(scale_factor-1) * l[0] + scale_factor * l[2], -(scale_factor-1) * l[1] + scale_factor * l[3])
-        angle = math.atan2(l[3]-l[1], l[2]-l[0])
         lines.append([p1, p2])
-        angles.append(angle)
 
     # Randomly sample two lines in order to find the horizontal and vertical axes
+    axis1, axis2 = None, None
     axis_vec_1, axis_vec_2 = None, None
     dot_product_tolerance = 0.1
     while True:
@@ -94,40 +99,90 @@ def board_localization(image, fen):
         # Dot product should be within tolerance of zero for lines to be orthogonal
         dot_product = vec1[0] * vec2[0] + vec1[1] * vec2[1]
         if (abs(dot_product) < dot_product_tolerance):
+            axis1, axis2 = i, j
             axis_vec_1, axis_vec_2 = vec1, vec2
             break
-        
-    line_group_1, line_group_2 = [], []
+
+    line_groups = [[], []]
     for i in range(len(lines)):
         l = lines[i]
         vec = (l[1][0] - l[0][0], l[1][1]-l[0][1])
         mag = math.sqrt(vec[0]*vec[0] + vec[1]*vec[1])
         vec = (vec[0]/mag, vec[1]/mag)
         dot_product = vec[0] * axis_vec_1[0] + vec[1] * axis_vec_1[1]
-        if (1-abs(dot_product) < dot_product_tolerance): line_group_1.append(i)
+        if (1-abs(dot_product) < dot_product_tolerance): line_groups[0].append(i)
         else:
             dot_product = vec[0] * axis_vec_2[0] + vec[1] * axis_vec_2[1]
-            if (1-abs(dot_product) < dot_product_tolerance): line_group_2.append(i)
+            if (1-abs(dot_product) < dot_product_tolerance): line_groups[1].append(i)
 
-    for i in line_group_1:
-        cv.line(im, lines[i][0], lines[i][1], (0, 255, 0), 3, cv.LINE_AA)
-    for i in line_group_2:
-        cv.line(im, lines[i][0], lines[i][1], (255, 0, 0), 3, cv.LINE_AA)
-        
-    # Compute the intersection points between the lines
+    # Compute the intersection points between the lines and use DBSCAN to reduce the number of lines in each group
+    new_line_groups = [[], []]
+    for k in [0, 1]:
+        point_to_line = {}
+        intersection_points = []
+        l2 = lines[axis2 if k == 0 else axis1]
+        for i in line_groups[k]:
+            l1 = lines[i]
+            inter_point = intersection_point(l1, l2)
+            if (inter_point != None and 0 <= inter_point[0] < width and 0 <= inter_point[1] < height):
+                x, y = inter_point
+                x, y = round(x), round(y)
+                point_to_line[(x, y)] = i
+                intersection_points.append((x, y))
+        intersection_points = np.asarray(intersection_points)
+        clustering = sklearn.cluster.DBSCAN(eps = 10, min_samples = 2, metric = "euclidean").fit(intersection_points)
+        seen = set()
+        for i in range(len(intersection_points)):
+            label = clustering.labels_[i]
+            if (label not in seen):
+                new_line_groups[k].append(point_to_line[tuple(intersection_points[i])])
+                seen.add(label)
+    line_groups = new_line_groups
+    del new_line_groups, clustering, intersection_points, point_to_line
+
+    for k in [0, 1]:
+        for i in line_groups[k]:
+            cv.line(im, lines[i][0], lines[i][1], (0, 255, 0) if k == 0 else (255, 0, 0), 2, cv.LINE_AA)
+
+    # Find all intersection points between line groups
     intersection_points = []
-    for i in line_group_1:
+    for i in line_groups[0]:
         l1 = lines[i]
-        for j in line_group_2:
+        for j in line_groups[1]:
             l2 = lines[j]
             inter_point = intersection_point(l1, l2)
             if (inter_point != None and 0 <= inter_point[0] < width and 0 <= inter_point[1] < height):
                 x, y = inter_point
                 x, y = round(x), round(y)
                 intersection_points.append((x, y))
-                cv.circle(im, (x, y), 5, (0,0,255), -1)
 
-    # cv.imshow("Canny Edge Detection", edges)
+    # Compute the homography matrix and fix current lines / find remaining lines
+    sample_1, sample_2 = [], []
+    while len(sample_1) != 2:
+        i = line_groups[0][np.random.randint(0, len(line_groups[0]))]
+        if (i not in sample_1): sample_1.append(i)
+    while len(sample_2) != 2:
+        i = line_groups[1][np.random.randint(0, len(line_groups[1]))]
+        if (i not in sample_2): sample_2.append(i)
+    point_sample = []
+    for i in sample_1:
+        l1 = lines[i]
+        for j in sample_2:
+            l2 = lines[j]
+            inter_point = intersection_point(l1, l2)
+            if (inter_point != None and 0 <= inter_point[0] < width and 0 <= inter_point[1] < height):
+                x, y = inter_point
+                x, y = round(x), round(y)
+                point_sample.append((x, y))
+                cv.circle(im, (x, y), 3, (0, 0, 255), 3, cv.LINE_AA)
+    p1, p2, p3, p4 = point_sample
+    min_x, max_x = min([p1[0], p2[0], p3[0], p4[0]]), max([p1[0], p2[0], p3[0], p4[0]])
+    min_y, max_y = min([p1[1], p2[1], p3[1], p4[1]]), max([p1[1], p2[1], p3[1], p4[1]])
+    s_x, s_y = 1, 1
+    H = compute_homography((max_x, min_y), (min_x, max_y), s_x, s_y)
+
+    # for piece in piece_data:
+    #     cv.rectangle(im, (piece[2][0], piece[2][1]), (piece[2][0] + piece[2][2], piece[2][1] + piece[2][3]), (0, 0, 255), 2, cv.LINE_AA)
     cv.imshow("Chessboard with Detected Lines", im)
     cv.imwrite("test.png", im)
     cv.waitKey()
@@ -137,10 +192,13 @@ if __name__ == "__main__":
     train_files = listdir("Data/train")
     while True:
         imname = str(int(np.random.randint(0, 4886)))
-        imname += (4-len(imname))*"0"+".png"
-        if (imname in train_files): break
-    im = cv.imread("Data/train/" + imname)
-    board_localization(im, None)
+        imname += (4-len(imname))*"0"
+        if (imname+".png" in train_files and imname+".json" in train_files): break
+    im = cv.imread("Data/train/" + imname + ".png")
+    metadata = json.load(open("Data/train/" + imname + ".json"))
+    fen = metadata['fen']
+    pieces = [(p['piece'], p['square'], p['box']) for p in metadata['pieces']]
+    board_localization(im, pieces)
     
     # train = True
     # filepath = "chess_classifier.keras"
