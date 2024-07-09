b305dec807e26752c324ea29e1eb8288bfdb09c7
derkyazn <131503815+derkyazn@users.noreply.github.com>
2024-07-09 15:21:58 -0700

    Added resnet model

--- pieceClassification.py
+++ pieceClassification.py
@@ -11,16 +11,59 @@ import cv2 as cv
 import tensorflow as tf
 import keras
 from keras._tf_keras.keras.applications.inception_v3 import InceptionV3
-# from keras.applications import InceptionV3
-# from keras.applications.InceptionV3 import InceptionV3
+from keras._tf_keras.keras.applications.resnet import ResNet101
 from keras import layers
 
 
+# Model for Occupancy Classification (Resnet 101)
+
+# Load test, train, and valid dataset.
+test_data_x, test_data_y = None, None
+train_data_x, train_data_y = None, None
+valid_data_x, valid_data_y = None, None
+
+# Model parameters
+batch_size = 128
+learning_rate = 0.001  
+
+# Make Base Model
+base_model = ResNet101(weights = 'imagenet', input_shape= (75, 75, 3), include_top = False, name = "pc-resnet101") # LOOK INTO 
+
+# Freeze base_model
+base_model.trainable = False
+
+# Do we need to also add our data to the inputs?
+inputs = keras.Input(shape=(75, 75, 3))
+
+# Calculate Outputs
+x = base_model(inputs, training=False)
+x = keras.layers.GlobalAveragePooling2D(name = "pc-pooling") * (base_model.output) 
+x = keras.Dense(1024, name = "pc-dense")(x)
+x = keras.Dense(256, name = "pc-dense2")(x)
+output = keras.layers.Dense(12, activation = "softmax", name = "pc-predictions")(x)
+
+#Make Model
+model = keras.Model(inputs = inputs, outputs = output)
+
+model.summary()
+
+# Compile, Train, And Save Model
+# model.compile(optimizer = keras.optimizers.Adam(learning_rate),
+#                     loss = keras.losses.CategoricalCrossentropy(),
+#                     metrics = ['accuracy'])
+epochs = 2 # How to change amount of epochs for classification head vs whole network? Also the piece classifaction should be double the occupancy classification
+# # model.fit(train_data_x, train_data_y,
+#                 validation_set = (valid_data_x, valid_data_y), epochs= epochs)
+# model.save(filepath, overwrite = True)
 
 
 
 
+# ---------------------------------------------------------------------------------
 
+
+
+# Model for Piece Classification (Inception v3)
 
 
 # Load test, train, and valid dataset.
@@ -33,17 +76,19 @@ batch_size = 128
 learning_rate = 0.001  
 
 # Make Base Model
-base_model = InceptionV3(weights = 'imagenet', input_shape= (75, 100, 3), include_top = False, name = "pc-inception-v3") # are we doing greyscale or color?
+base_model = InceptionV3(weights = 'imagenet', input_shape= (75, 75, 3), include_top = False, name = "pc-inception-v3")
 
 # Freeze base_model
 base_model.trainable = False
 
 # Do we need to also add our data to the inputs?
-inputs = keras.Input(shape=(75, 100, 3))
+inputs = keras.Input(shape=(75, 75, 3))
 
 # Calculate Outputs
 x = base_model(inputs, training=False)
 x = keras.layers.GlobalAveragePooling2D(name = "pc-pooling") * (base_model.output) 
+x = keras.Dense(1024, name = "pc-dense")(x)
+x = keras.Dense(256, name = "pc-dense2")(x)
 output = keras.layers.Dense(12, activation = "softmax", name = "pc-predictions")(x)
 
 #Make Model
@@ -51,20 +96,14 @@ model = keras.Model(inputs = inputs, outputs = output)
 
 model.summary()
 
-
 # Compile, Train, And Save Model
-
-
-model.compile(optimizer = keras.optimizers.Adam(learning_rate),
-                    loss = keras.losses.CategoricalCrossentropy(),
-                    metrics = ['accuracy'])
-
-
+# model.compile(optimizer = keras.optimizers.Adam(learning_rate),
+#                     loss = keras.losses.CategoricalCrossentropy(),
+#                     metrics = ['accuracy'])
 epochs = 2 # How to change amount of epochs for classification head vs whole network? Also the piece classifaction should be double the occupancy classification
-model.fit(train_data_x, train_data_y,
-                validation_set = (valid_data_x, valid_data_y), epochs= epochs)
-
-model.save(filepath, overwrite = True)
+# # model.fit(train_data_x, train_data_y,
+#                 validation_set = (valid_data_x, valid_data_y), epochs= epochs)
+# model.save(filepath, overwrite = True)
 
 
 
