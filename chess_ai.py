9a5449ab6177c7f587f0e88631e03dbe1f6532a8
derkyazn <131503815+derkyazn@users.noreply.github.com>
2024-07-21 02:14:51 -0700

    working on merging all the steps. not done yet

--- chess_ai.py
+++ chess_ai.py
@@ -1,6 +1,14 @@
 from stockfish import Stockfish
 import chess
 import numpy as np
+import cv2
+import model
+import keras
+
+
+
+
+
 
 # TODO:
 # Chess Engine still needs testing but it seems viable so far?
@@ -12,6 +20,7 @@ skill_level = 12
 depth = 15
 min_think_time = 15
 elo = 1350
+model = keras.models.load_model("occupancy_classifier.keras")
 
 # Stockfish & board objects
 stockfish = Stockfish(path = "stockfish/stockfish-windows-x86-64-avx2.exe",
@@ -54,10 +63,80 @@ while True:
         #    move = moves[i]
         #if (choose_randomly): move = np.random.choice(moves, )
     else: # Player's turn
-        while True:
-            move = input("What is your move (in algebraic notation)? ")
-            if (stockfish.is_move_correct(move)): break
-            print("Move invalid. Please try again.")
+
+        img_captured_corners = None
+        frame2 = None
+        while True: # Change while loop condition later
+
+            cam = cv2.VideoCapture(0)
+            value, frame = cam.read()
+
+            frame2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            GRID = (7, 7)
+
+
+
+            found, corners = cv2.findChessboardCorners(frame2, GRID, None)
+            print("found: ", found, " corners: ", corners)
+            cv2.imshow("Camera View", frame)
+
+            
+            if found:
+                img_captured_corners = cv2.drawChessboardCorners(frame, GRID, corners, found)
+                cv2.imshow("Camera View", img_captured_corners)
+                break
+
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        
+        # Question! What is the purpose of gather_piece_data? #does this only work if you were given the metadata?
+        # oh i see, when actually running this images should be the only thing it returns?
+        images, piece_images, piece_labels, empty_labels = model.board_localization(image= frame2, corners= img_captured_corners, white_view= True, inner_grid= True, cw= 100, ch= 100, gather_piece_data= False ) # Assumes that it will always be white view
+
+        str_labels = ["Empty", "Not Empty"]
+        occupied_tiles = []
+        all_occupancies = []
+        for i in images:
+      
+            # Input img ---> img shape (100,100,3)
+            img = np.expand_dims(img, 0) # ---> img shape now (1,100,100,3)
+            # pred = model(img) ---> pred shape (1, 2)
+            pred = np.reshape(pred, -1) # ---> pred shape now (2)
+            # pred[0] = probability of 0th class, pred[1] = probability of 1st class
+            # 0 class = empty, 1 class = occupied
+            label = np.argmax(pred)
+            all_occupancies.append(label)
+            if (label == 1):
+                occupied_tiles.append(label)
+            
+        
+        str_labels = "PRNBQKprnbqk"
+        all_pieces = []
+        for i in occupied_tiles:
+            # Input img ---> img shape (100,100,3)
+            img = np.expand_dims(img, 0) # ---> img shape now (1,100,100,3)
+            # pred = model(img) ---> pred shape (1, 12)
+            pred = np.reshape(pred, -1) # ---> pred shape now (12)
+            # pred[i] = probability of ith class
+            # Classes are in order "PRNBQKprnbqk"
+            all_pieces.append(label)
+
+
+    
+
+        # loop through the results and make a fenstring
+        # then compare this fenstring with the previous fen string??
+        # because we need to input the movement, not the board, i need to find out how to calculate the movment 
+
+
+
+        move = input("What is your move (in algebraic notation)? ")
+        if (stockfish.is_move_correct(move)): break
+        print("Move invalid. Please try again.")
+
+
     # Update the board state.
     board.push(chess.Move.from_uci(move))
     current_move = not current_move
