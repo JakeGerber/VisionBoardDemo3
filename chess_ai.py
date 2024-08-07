09af3f0a95019778cf98d9cceeef70ebee371634
derkyazn <131503815+derkyazn@users.noreply.github.com>
2024-08-07 01:23:23 -0700

    IT WORKS WOOOOOOOOOO

--- chess_ai.py
+++ chess_ai.py
@@ -68,14 +68,17 @@ stockfish.set_skill_level(skill_level)
 board = chess.Board()
 move_number = 1
 print("Stockfish Setup Successful")
+# print(stockfish.get_board_visual(not is_white))
+print(board)
 
 current_move = True # True = white, False = black
 while True:
+    
     # Display the current state of the game.
     stockfish.set_fen_position(board.fen())
     wdl_stats = stockfish.get_wdl_stats()
     if (wdl_stats != None): print("Predicted outcomes:\nWin: %.3f, Draw: %.3f, Lose: %.3f" % (wdl_stats[0]/1000, wdl_stats[1]/1000, wdl_stats[2]/1000))
-    print(stockfish.get_board_visual(not is_white))
+    # print(stockfish.get_board_visual(not is_white))
     
     # Check for win/draw/loss conditions.
     if (board.is_checkmate()):
@@ -89,6 +92,7 @@ while True:
 
     # Ask the player or stockfish for their move.
     if (current_move == is_white):  # Stockfish's turn (Currently W and Stockfish W or currently B and Stockfish B)
+        print("--------------AI's Turn--------------")
         move = stockfish.get_best_move()
         #moves = stockfish.get_top_moves(len(move_choice_dist))
         #choose_randomly = True
@@ -97,6 +101,7 @@ while True:
         #if (choose_randomly): move = np.random.choice(moves, )
 
     else: # Player's turn
+        print("---------------Player's Turn-------------------")
 
         ############## STEP 1) CHESS BOARD CORNER DETECTION ##################
         # GOAL: Return the locations of the corners of every tile on the board & return the source image 
@@ -130,14 +135,14 @@ while True:
 
                 # Should i just save "corners" or "img_captured_corners"
                 found, cornersList = cv2.findChessboardCorners(frame2, GRID, None)
-                print("found: ", found, " corners: ", cornersList)
+                # print("found: ", found, " corners: ", cornersList)
                 cv2.imshow("Camera View", frame)
-                print("joe biden 3")
+                # print("joe biden 3")
 
                 if found:
                     img_captured_corners = cv2.drawChessboardCorners(frame, GRID, cornersList, found) # Can get rid of this in the final demo if we don't want to show the visualization
                     cv2.imshow("Camera View", img_captured_corners)
-                    print("FOUND CORNERS")
+                    # print("FOUND CORNERS")
                     break
                 
 
@@ -157,21 +162,21 @@ while True:
 
             # Should only return 64 cut up images, the others should be empty arrays 
 
-            print(cornersList[0])
-            print(cornersList[0])
-            print("-----------------------")
-            print(cornersList[0][0])
-            print(len(cornersList[0][0]))
+            # print(cornersList[0])
+            # print(cornersList[0])
+            # print("-----------------------")
+            # print(cornersList[0][0])
+            # print(len(cornersList[0][0]))
 
             formattedCornersList = []
             for i in cornersList:
-                print(i)
-                print("boob")
+                # print(i)
+                # print("boob")
                 formattedCornersList.append([i[0][0], i[0][1]])
-                print(i[0])
-                print("gay")
-                print(i[0][0])
-            print(formattedCornersList)
+                # print(i[0])
+                # print("gay")
+                # print(i[0][0])
+            # print(formattedCornersList)
 
             images, piece_images, piece_labels, empty_labels = model.board_localization(image= fullImage, piece_data=[], corners= formattedCornersList
                                                                                         , white_view= True, inner_grid= True, cw= 100, ch= 100,
@@ -200,10 +205,10 @@ while True:
                 if (label == 1):
                     occupied_tiles.append(label)
 
-                print("all occupancies")
-                print(all_occupancies)
-                print("occupied tiles")
-                print(occupied_tiles)
+                # print("all occupancies")
+                # print(all_occupancies)
+                # print("occupied tiles")
+                # print(occupied_tiles)
 
 
 
@@ -225,8 +230,8 @@ while True:
                     label = np.argmax(pred)
                     all_pieces.append(label)
             
-            print("all pieces")
-            print(all_pieces)
+            # print("all pieces")
+            # print(all_pieces)
 
 
         
@@ -243,13 +248,56 @@ while True:
 
             for occ in all_occupancies:
                 if occ == 0: # if empty
-                    new_detected_board_state.append("0")
+                    new_detected_board_state.append("E")
                 else: # if not empty
                     new_detected_board_state.append(all_pieces[piece_iterator])
                     piece_iterator += 1
                 
-            print("new detected board state")
-            print(new_detected_board_state)
+            # print("new detected board state")
+            # print(new_detected_board_state)
+            # print(board.legal_moves)
+            joe = list(board.legal_moves)
+            # print(joe)
+            # print("joe")
+            # print(type(chess.Move.uci((joe[0]))))
+            # print(chess.Move.uci((joe[0])))
+            # # print(stockfish.get_board_visual(not is_white))
+            # print(board)
+            board.push_uci(chess.Move.uci(joe[0]))
+            # print("-------------------------------------")
+            # # print(stockfish.get_board_visual(not is_white))
+            # print(board)
+            # # input("press the Enter key to continue: ")
+
+
+#             [0, 4, 'E', 'E', 'E', 9, 'E', 7, 4, 3, 2, 'E', 'E', 'E', 'E', 8, 0, 'E', 4, 4, 'E', 'E', 9, 9, 'E', 0, 'E', 4, 0, 'E', 0, 9, 'E', 'E', 'E', 'E', 0, 3, 4, 5, 'E', 'E', 2, 2, 2, 4, 2, 0, 'E', 'E', 2, 2, 4, 'E', 2, 2, 
+# 0, 0, 4, 2, 2, 'E', 'E', 4]
+
+            str_labels = "PRNBQKprnbqk"
+            old_board = [
+                        7, 8, 9, 10, 11, 9, 8, 7,
+                        6, 6, 6, 6, 6, 6, 6, 6,
+                        "E", "E", "E", "E", "E", "E", "E", "E",
+                        "E", "E", "E", "E", "E", "E", "E", "E",
+                        "E", "E", "E", "E", "E", "E", "E", "E",
+                        "E", "E", "E", "E", "E", "E", "E", "E",
+                        0, 0, 0, 0, 0, 0, 0, 0,
+                        1, 2, 3, 4, 5, 3, 2, 1
+                        ]
+            new_board = [
+                        7, 8, 9, 10, 11, 9, 8, 7,
+                        6, 6, 6, 6, 6, 6, 6, 6,
+                        "E", "E", "E", "E", "E", "E", "E", "E",
+                        "E", "E", "E", "E", "E", "E", "E", "E",
+                        0, "E", "E", "E", "E", "E", "E", "E",
+                        "E", "E", "E", "E", "E", "E", "E", "E",
+                        "E", 0, 0, 0, 0, 0, 0, 0,
+                        1, 2, 3, 4, 5, 3, 2, 1
+                        ]
+            
+            current_board_state = old_board
+            new_detected_board_state = new_board
+
 
 
         
@@ -260,6 +308,10 @@ while True:
             for i in range(64):
                 if current_board_state[i] != new_detected_board_state[i]:
                     index_difference.append(i)
+
+            print("index difference")
+            print(index_difference)
+
 
             # if more than two differences, rescane (can there be more than movement per turn? can u hop like checkers? i forgor)
 
@@ -274,24 +326,30 @@ while True:
 
             # board_location_dictionary = {1 : "h1", 2: "g1", 3: "f1", 4: "e1", 5: "d1", 6: "c1", 7: "b1", 8:"a1"}
             board_location_dictionary = {
-                    1: "a1", 2: "b1", 3: "c1", 4: "d1", 5: "e1", 6: "f1", 7: "g1", 8: "h1",
-                    9: "a2", 10: "b2", 11: "c2", 12: "d2", 13: "e2", 14: "f2", 15: "g2", 16: "h2",
-                    17: "a3", 18: "b3", 19: "c3", 20: "d3", 21: "e3", 22: "f3", 23: "g3", 24: "h3",
-                    25: "a4", 26: "b4", 27: "c4", 28: "d4", 29: "e4", 30: "f4", 31: "g4", 32: "h4",
-                    33: "a5", 34: "b5", 35: "c5", 36: "d5", 37: "e5", 38: "f5", 39: "g5", 40: "h5",
-                    41: "a6", 42: "b6", 43: "c6", 44: "d6", 45: "e6", 46: "f6", 47: "g6", 48: "h6",
-                    49: "a7", 50: "b7", 51: "c7", 52: "d7", 53: "e7", 54: "f7", 55: "g7", 56: "h7",
-                    57: "a8", 58: "b8", 59: "c8", 60: "d8", 61: "e8", 62: "f8", 63: "g8", 64: "h8"}
+                            0: "a8", 1: "b8", 2: "c8", 3: "d8", 4: "e8", 5: "f8", 6: "g8", 7: "h8",
+                            8: "a7", 9: "b7", 10: "c7", 11: "d7", 12: "e7", 13: "f7", 14: "g7", 15: "h7",
+                            16: "a6", 17: "b6", 18: "c6", 19: "d6", 20: "e6", 21: "f6", 22: "g6", 23: "h6",
+                            24: "a5", 25: "b5", 26: "c5", 27: "d5", 28: "e5", 29: "f5", 30: "g5", 31: "h5",
+                            32: "a4", 33: "b4", 34: "c4", 35: "d4", 36: "e4", 37: "f4", 38: "g4", 39: "h4",
+                            40: "a3", 41: "b3", 42: "c3", 43: "d3", 44: "e3", 45: "f3", 46: "g3", 47: "h3",
+                            48: "a2", 49: "b2", 50: "c2", 51: "d2", 52: "e2", 53: "f2", 54: "g2", 55: "h2",
+                            56: "a1", 57: "b1", 58: "c1", 59: "d1", 60: "e1", 61: "f1", 62: "g1", 63: "h1"
+                        }
+
 
             player_move = ""
 
             if len(index_difference) == 2:
-                if new_detected_board_state[index_difference[0]].islower():
-                    player_move.append(board_location_dictionary[index_difference[0]])
-                    player_move.append(board_location_dictionary[index_difference[1]])
-                elif new_detected_board_state[index_difference[1]].islower():
-                    player_move.append(board_location_dictionary[index_difference[1]])
-                    player_move.append(board_location_dictionary[index_difference[0]])
+                if new_detected_board_state[index_difference[0]] == 0:
+                    player_move = player_move + board_location_dictionary[index_difference[1]]
+                    player_move = player_move + board_location_dictionary[index_difference[0]]
+                elif new_detected_board_state[index_difference[1]] == 0:
+                    player_move = player_move + board_location_dictionary[index_difference[0]]
+                    player_move = player_move + board_location_dictionary[index_difference[1]]
+        
+            print("player move")
+            print(player_move)
+            move = player_move
 
 
 
@@ -299,13 +357,23 @@ while True:
             
 
             ############## STEP 6) CORRECTING INCORRECT CHESS BOARD ##################
-            if (stockfish.is_move_correct(player_move)):
+            # if (stockfish.is_move_correct(player_move)):
+            if (chess.Move.from_uci(player_move) in board.legal_moves):
                 board_scan = True
+                
+
+                # print("------------------ GOT HERE ----------------------------------")
             else:
                 fail_scan_coutner += 1
+                print("----------------------- POOOOP ---------------------------------")
             if fail_scan_coutner >= 5:
                 print("Move invalid. Please try again.")
                 fail_scan_coutner = 0
+
+
+        # print(board.legal_moves)
+        # 
+
 
 
 
@@ -314,6 +382,8 @@ while True:
 
     # Update the board state.
     board.push(chess.Move.from_uci(move))
+    #print(stockfish.get_board_visual(not is_white))
+    print(board)
     current_move = not current_move
     move_number += 1
 
